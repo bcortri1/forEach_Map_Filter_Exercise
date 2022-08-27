1065997c92a7f451be987dda5dd1d30b3b0b17cf
Blake C <bcortri1@kent.edu>
2022-08-27 10:21:56 -0400

    double function complete

--- forEach.js
+++ forEach.js
@@ -1,6 +1,6 @@
-function doubleValues(arr){
-    arr.array.forEach(function(item,index) {
+function doubleValues(arr) {
+    arr.forEach(function(item,index){
         arr[index]= item*2;
-    });
+    })
     return arr;
-}
+};
